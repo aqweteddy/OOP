4a14fab47a30b76b97b063a4ea35ed24c4882695
aqweteddy <keddy940199@gmail.com>
2018-05-07 11:42:28 +0800

    HW4 update

--- HW4/Bag.h
+++ HW4/Bag.h
@@ -1,3 +1,10 @@
+#include <iostream>
+#include <set>
+#include <map>
+
+using std::cout;
+using std::endl;
+
 template <class ItemType>
 class Bag {
     template <class T>
@@ -5,36 +12,185 @@ class Bag {
     template <class T>
     friend void subtract(Bag<T>&, Bag<T>&, Bag<T>&);
 
-  private:
+   private:
     struct list {
         list* next;
         list* prev;
         ItemType item;
     };
+
     list* head;
     list* now;
     int bag_size;
-  
+
    public:
-    Bag();
-    ~Bag();
-    Bag(const Bag&);
-    Bag& operator=(const Bag&);
+    Bag() {
+        head = new list;
+        head->next = head;
+        head->prev = head;
+        now = NULL;
+        bag_size = 0;
+        cout << "Bag constructed" << endl;
+    }
 
-    bool empty() const;
-    int size() const;
-    int uniqueSize() const;
-    bool insert(const ItemType&);
-    int erase(const ItemType&);
-    int eraseAll(const ItemType&);
-    bool contains(const ItemType&) const;
-    int count(const ItemType&) const;
+    ~Bag() {
+        clear();
+        delete (head);
+        cout << "Bag destructed" << endl;
+    }
 
-    void start();
-    void next();
-    bool ended() const;
-    const ItemType& currentValue() const;
-    int currentCount() const;
+    Bag(const Bag& b) {
+        cout << "Bag copy constructor" << endl;
+        head = new list;
+        bag_size = 0;
+        now = NULL;
+        head->next = head;
+        head->prev = head;
 
-    void clear();
+        for (list* i = b.head->next; i != b.head; i = i->next) insert(i->item);
+    }
+
+    Bag& operator=(const Bag& b) {
+        head = new list;
+        bag_size = 0;
+        now = NULL;
+        head->next = head;
+        head->prev = head;
+
+        for (list* i = b.head->next; i != b.head; i = i->next) {
+            insert(i->item);
+        }
+        return (*this);
+    }
+
+    bool empty() const { return head->next == head; }
+
+    int size() const { return bag_size; }
+
+    int uniqueSize() const {
+        std::set<ItemType> s;
+
+        for (list* cur = head->next; cur != head; cur = cur->next) {
+            s.insert(cur->item);
+        }
+        return s.size();
+    }
+
+    bool insert(const ItemType& item) {
+        list* tmp = new list;
+        list* end = head->prev;
+        end->next = tmp;
+        head->prev = tmp;
+        tmp->item = item;
+        tmp->next = head;
+        tmp->prev = end;
+        ++bag_size;
+        return 1;
+    }
+
+    int erase(const ItemType& it) {
+        int cnt = 1;
+
+        for (list *cur = head->next; cur != head; cur = cur->next, ++cnt) {
+            if (cur->item == it) {
+                cur->prev->next = cur->next;
+                cur->next->prev = cur->prev;
+                delete (cur);
+                return cnt;
+            }
+        }
+        return -1;
+    }
+
+    int eraseAll(const ItemType& it) {
+        int cnt = 0;
+        list * cur = head -> next;
+
+        while(cur != head){
+            if(cur -> item == it){
+                ++cnt;
+                list * old = cur;
+                cur = cur -> next;
+                old -> prev -> next = old -> next;
+                old -> next -> prev = old -> prev;
+                delete(old);
+            } else cur = cur -> next;
+        }
+        return cnt;
+    }
+
+    bool contains(const ItemType& it) const {
+        for (list* cur = head->next; cur != head; cur = cur->next) {
+            if (cur->item == it) return true;
+        }
+        return false;
+    }
+
+    int count(const ItemType& it) const {
+        int cnt = 0;
+
+        for (list* cur = head->next; cur != head; cur = cur->next) {
+            if (cur->item == it) ++cnt;
+        }
+        return cnt;
+    }
+
+    void start() {
+        if (head->next == head) {
+            cout << "Please input item" << endl;
+            return;
+        }
+        now = head->next;
+    }
+
+    void next() {
+        if (ended()) {
+            cout << "Please initializes" << endl;
+            return;
+        }
+        now = now->next;
+        if (now == head) now = NULL;
+    }
+
+    bool ended() const { return now == NULL; }
+
+    const ItemType& currentValue() const {
+        if (now == NULL) {
+            cout << "Please start finger" << endl;
+            return head->item;
+        }
+        return now->item;
+    }
+
+    int currentCount() const {
+        int i = 1;
+        for (list *cur = head->next; cur != now; cur = cur->next, ++i)
+            ;
+        return i;
+    }
+
+    void clear() {
+        for (auto cur = head->next; cur != head; cur = cur->next) delete (cur);
+        head->next = head;
+        head->prev = head;
+    }
 };
+
+template <class ItemType>
+void combine(Bag<ItemType>& bag1, Bag<ItemType>& bag2, Bag<ItemType>& result) {
+    for (auto i = bag2.head->next; bag2.head != i; i = i->next)
+        result.insert(i->item);
+    for (auto i = bag1.head->next; bag1.head != i; i = i->next)
+        result.insert(i->item);
+}
+
+template <class ItemType>
+void subtract(Bag<ItemType>& bag1, Bag<ItemType>& bag2, Bag<ItemType>& result) {
+    std::map<ItemType, int> dict;
+
+    for (auto i = bag1.head->next; i != bag1.head; i = i->next) ++dict[i->item];
+    for (auto i = bag2.head->next; i != bag2.head; i = i->next) --dict[i->item];
+    for (auto i : dict) {
+        for (int j = 0; j < i.second; ++j) result.insert(i.first);
+    }
+}
--- HW4/main.cpp
+++ HW4/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include "Bag.cpp"
+#include "Bag.h"
 
 using std::cout;
 using std::endl;
@@ -13,6 +13,7 @@ int main() {
     cout << a.ended() << endl;  //若手指還未指向任和東西則回傳true//印1
 
     cout << "-------Test for finger-------" << endl;
+    cout << a.currentValue() << endl;  //輸出Please start finger和空白行
     a.next();   //還未將手指初始化就呼叫next()//印Please initializes
     a.start();  //初始化手指，指到其中一個物品上//若包包為空，印Please input
                 // item程式不能壞掉
@@ -34,11 +35,11 @@ int main() {
     a.insert("a3");
     cout << a_copy.size() << endl;  //還是印2
 
-    Bag<string> a_assignment;
+    Bag<string> a_assignment;  // Bag constrcucted
     a_assignment.insert("assignment1");
-    a_assignment =
-        a;  //把a指派給a_assignment，a_assignment原來的舊有值不該存在/Bag
-            // assignment operator/印
+    a_assignment = a;
+    //把a指派給a_assignment，a_assignment原來的舊有值不該存在/Bag
+    // assignment operator/印
     cout << a_assignment.size() << endl;  //印3
     a_assignment.start();
     cout << a_assignment.currentValue() << endl;  //印a1
@@ -54,9 +55,12 @@ int main() {
     b.insert(20);
     b.insert(30);
     b.insert(10);
-    cout << b.size() << endl;        //包包中物品總數//印6
-    cout << b.uniqueSize() << endl;  //包包中相異物品數//印3
+    b.insert(90);
+
+    cout << b.size() << endl;        //包包中物品總數//印7
+    cout << b.uniqueSize() << endl;  //包包中相異物品數//印4
     cout << b.erase(10) << endl;  //刪除(10)，回傳是第幾個物品//印1
+    cout << b.erase(90) << endl;  //印6
     cout << b.eraseAll(20) << endl;  //刪除所有(20)，回傳刪除的總數//印3
     cout << b.contains(20) << endl;  //包包中是否包含(20)//印0
     cout << b.count(10) << endl;     //包包中(10)的數量//印1
@@ -74,20 +78,20 @@ int main() {
     c2.insert("goose");
     c2.insert("chicken");
     c2.insert("chicken");
-
-    combine(
-        c1, c2,
-        c_result);  // c_result中
-                    // 要有c1與c2中所有物品，在這裡就沒有規定順序，只要物品數量正確即可
-                    // c_result中要有3個"duck", 3個"goose", 4個"chicken"
+    
+    // c_result中
+    // 要有c1與c2中所有物品，在這裡就沒有規定順序，只要物品數量正確即可
+    // c_result中要有3個"duck", 3個"goose", 4個"chicken"
+    combine(c1, c2, c_result);
     cout << "duck: " << c_result.count("duck") << endl;
     cout << "goose: " << c_result.count("goose") << endl;
     cout << "chichen: " << c_result.count("chicken") << endl;
 
     c_result.clear();
-    subtract(c1, c2,
-             c_result);  //在這裡就沒有規定順序，只要物品數量正確即可
-                         // c_result中要有1個"duck", 0個"goose", 0個"chicken"
+
+    //在這裡就沒有規定順序，只要物品數量正確即可
+    // c_result中要有1個"duck", 0個"goose", 0個"chicken"
+    subtract(c1, c2, c_result);
     cout << "duck: " << c_result.count("duck") << endl;
     cout << "goose: " << c_result.count("goose") << endl;
     cout << "chichen: " << c_result.count("chicken") << endl;
